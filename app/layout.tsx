3926df76d2c84c9add31c2af351441d0ea6a679e
rzkysyftra <riskisyafutrajaya@gmail.com>
2025-08-15 20:49:56 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "./client-layout";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
