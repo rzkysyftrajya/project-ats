65410c662182e32c3388e71cd32395d550d9596e
rzkysyftra <riskisyafutrajaya@gmail.com>
2025-08-15 20:36:16 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "./client-layout";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
